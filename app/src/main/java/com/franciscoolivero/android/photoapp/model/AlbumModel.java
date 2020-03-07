300192c2056824aba34e6b383988bc62fe88a925
Francisco Olivero <franj.olivero@gmail.com>
2020-03-07 19:34:50 -0300

    Created AlbumModel object with Serialized names for retrofit use

--- app/src/main/java/com/franciscoolivero/android/photoapp/model/AlbumModel.java
+++ app/src/main/java/com/franciscoolivero/android/photoapp/model/AlbumModel.java
@@ -1,4 +1,24 @@
 package com.franciscoolivero.android.photoapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class AlbumModel {
+    @SerializedName("id")
+    private int album_id;
+    @SerializedName("title")
+    private String album_title;
+
+    public AlbumModel(int album_id, String album_title) {
+        this.album_id = album_id;
+        this.album_title = album_title;
+    }
+
+    public int getAlbum_id() {
+        return album_id;
+    }
+
+    public String getAlbum_title() {
+        return album_title;
+    }
+
 }
