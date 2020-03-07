2adb928183ea3eb5f1a436b6027eda94894a6da0
Francisco Olivero <franj.olivero@gmail.com>
2020-03-07 19:49:41 -0300

    Created ViewModel class for Albums screen and moved MainActivity to view package

--- app/src/main/java/com/franciscoolivero/android/photoapp/viewmodel/AlbumsViewModel.java
+++ app/src/main/java/com/franciscoolivero/android/photoapp/viewmodel/AlbumsViewModel.java
@@ -1,4 +1,13 @@
 package com.franciscoolivero.android.photoapp.viewmodel;
 
-public class AlbumsViewModel {
+import java.util.List;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AlbumsViewModel extends ViewModel {
+
+    public MutableLiveData<List<AlbumsViewModel>> albumLiveData = new MutableLiveData<>();
+    public MutableLiveData<Boolean> albumLoadErrorLiveData = new MutableLiveData<>();
+    public MutableLiveData<Boolean> albumIsLoadingLiveData = new MutableLiveData<>();
 }
