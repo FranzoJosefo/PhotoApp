287339b788e268e40352b88a062d1a3af0146238
Francisco Olivero <franj.olivero@gmail.com>
2020-03-08 23:25:23 -0300

    Inject Networkservice into viewModel, update wrong MutableLiveData variable, Added fetch methods for Albums and Photos (needed for album cover).

--- app/src/main/java/com/franciscoolivero/android/photoapp/viewmodel/AlbumsViewModel.java
+++ app/src/main/java/com/franciscoolivero/android/photoapp/viewmodel/AlbumsViewModel.java
@@ -1,20 +1,84 @@
 package com.franciscoolivero.android.photoapp.viewmodel;
 
+import com.franciscoolivero.android.photoapp.data.NetworkService;
 import com.franciscoolivero.android.photoapp.di.DaggerApiComponent;
+import com.franciscoolivero.android.photoapp.model.AlbumModel;
+import com.franciscoolivero.android.photoapp.model.PhotoModel;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class AlbumsViewModel extends ViewModel {
 
-    public MutableLiveData<List<AlbumsViewModel>> albumLiveData = new MutableLiveData<>();
+    public MutableLiveData<List<AlbumModel>> albumsLiveData = new MutableLiveData<>();
     public MutableLiveData<Boolean> albumLoadErrorLiveData = new MutableLiveData<>();
     public MutableLiveData<Boolean> albumIsLoadingLiveData = new MutableLiveData<>();
+
+    @Inject
+    public NetworkService networkService;
+
+    private CompositeDisposable disposable = new CompositeDisposable();
 
     public AlbumsViewModel() {
         super();
         DaggerApiComponent.create().inject(this);
+    }
+
+    public void fetchAlbums() {
+        albumIsLoadingLiveData.setValue(true);
+
+        disposable.add(
+                networkService.getAlbums()
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableSingleObserver<List<AlbumModel>>() {
+                            @Override
+                            public void onSuccess(List<AlbumModel> albumModels) {
+                                albumsLiveData.setValue(albumModels);
+                                albumLoadErrorLiveData.setValue(false);
+                                albumIsLoadingLiveData.setValue(false);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                albumLoadErrorLiveData.setValue(true);
+                                albumIsLoadingLiveData.setValue(false);
+                                e.printStackTrace();
+                            }
+                        })
+        );
+    }
+
+    public void fetchPhotos(int albumId, MutableLiveData<List<PhotoModel>> photosLiveData) {
+        disposable.add(
+                networkService.getPhotos(albumId)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableSingleObserver<List<PhotoModel>>() {
+                            @Override
+                            public void onSuccess(List<PhotoModel> photoModels) {
+                                photosLiveData.setValue(photoModels);
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                e.printStackTrace();
+                            }
+                        })
+        );
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        disposable.clear();
     }
 }
