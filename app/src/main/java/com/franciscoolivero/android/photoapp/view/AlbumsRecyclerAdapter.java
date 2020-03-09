a7477a0c7c089d5b23f5d93f02e36cf2d4ab6101
Francisco Olivero <franj.olivero@gmail.com>
2020-03-08 23:23:09 -0300

    Added logic in the AlbumRecyclerAdapter to load images as cover for the album using Observers.

--- app/src/main/java/com/franciscoolivero/android/photoapp/view/AlbumsRecyclerAdapter.java
+++ app/src/main/java/com/franciscoolivero/android/photoapp/view/AlbumsRecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.franciscoolivero.android.photoapp.view;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +9,13 @@ import android.widget.TextView;
 
 import com.franciscoolivero.android.photoapp.R;
 import com.franciscoolivero.android.photoapp.model.AlbumModel;
+import com.franciscoolivero.android.photoapp.model.PhotoModel;
+import com.franciscoolivero.android.photoapp.utils.ImageUtil;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,8 +24,11 @@ public class AlbumsRecyclerAdapter extends RecyclerView.Adapter<AlbumsRecyclerAd
 
     private List<AlbumModel> albumsList;
 
-    public AlbumsRecyclerAdapter(List<AlbumModel> albumsList) {
+    private Context context;
+
+    public AlbumsRecyclerAdapter(List<AlbumModel> albumsList, Context context) {
         this.albumsList = albumsList;
+        this.context = context;
     }
 
     public void updateAlbums(List<AlbumModel> newAlbums) {
@@ -33,7 +40,7 @@ public class AlbumsRecyclerAdapter extends RecyclerView.Adapter<AlbumsRecyclerAd
     @NonNull
     @Override
     public AlbumViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_cell_albums_page,parent, false);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_cell_albums_page, parent, false);
         return new AlbumViewHolder(itemView);
     }
 
@@ -54,14 +61,22 @@ public class AlbumsRecyclerAdapter extends RecyclerView.Adapter<AlbumsRecyclerAd
         @BindView(R.id.grid_cell_album_name_text_view)
         TextView albumName;
 
+        public MutableLiveData<List<PhotoModel>> photoModels = new MutableLiveData<>();
+
         public AlbumViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            setupObservers();
+        }
+
+        private void setupObservers() {
+            photoModels.observe((MainActivity) context, photoModels -> ImageUtil.loadImage(
+                    albumCoverImage, photoModels.get(0).getPhotoThumbnailUrl(), ImageUtil.getProgressDrawable(albumCoverImage.getContext())));
         }
 
         public void bind(AlbumModel album) {
+            ((MainActivity) context).getViewModel().fetchPhotos(album.getAlbum_id(), photoModels);
             albumName.setText(album.getAlbum_title());
-
         }
     }
 }
